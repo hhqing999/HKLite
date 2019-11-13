f76edb0da02602509b754d69687a52fedc39fcde
hhqing999 <hhqing999@163.com>
2019-11-13 17:58:21 +0800

    Move code files from HKLite.Sqlce.Wince to HKLite.Sqlite.Windows

---
+++ HKLite.SqlServer/DALFactory.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+using System.ComponentModel;
+namespace HKLite
+{
+    /* 作者：何宏卿
+     * 日期：2015-09-29
+     * 描述：创建DALayer工厂类
+     */
+    /// <summary>
+    /// 创建DALayer工厂类
+    /// </summary>
+    public abstract class DALFactory
+    {
+        /// <summary>
+        /// 创建DALLayer 实例
+        /// </summary>
+        /// <param name="connectionString">连接字符串</param>
+        /// <param name="entityDllName">实体集名称</param>
+        /// <param name="longConnection">是否长连接</param>
+        /// <returns></returns>
+        public static IDALayer CreateLayer(string connectionString, string entityDllName, bool longConnection)
+        {
+            string dbName = entityDllName.Replace(".", "_").ToLower();           
+            return new DALayer(connectionString, entityDllName, longConnection, new DbProvider());
+        }
+
+        /// <summary>
+        /// 测试连接字符串能否连接到数据库
+        /// </summary>
+        /// <param name="connectionString"></param>
+        /// <param name="msg"></param>
+        /// <returns></returns>
+        public static bool TestConnection(string connectionString, ref string msg)
+        {
+            if (string.IsNullOrEmpty(connectionString.Trim()))
+            {
+                msg = "连接字符串为空";
+                return false;
+            }
+            try
+            {
+                IDbProvider iprovider = new DbProvider();
+                object obj = SqlHelper.ExecuteScalar(iprovider.CreateConnection(connectionString), false, "select 1");
+                if (obj == null || obj == DBNull.Value)
+                    return false;
+                if (Convert.ToInt32(obj) == 1)
+                    return true;
+                return false;
+            }
+            catch (Exception ex)
+            {
+                msg = ex.Message;
+                return false;
+            }
+        }
+
+    }
+}
---
+++ HKLite.Sqlce.Wince/DALFactory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+using System.ComponentModel;
+namespace HKLite
+{
+    /* 作者：何宏卿
+     * 日期：2014-12-23
+     * 描述：创建DALayer工厂类
+     */
+    /// <summary>
+    /// 创建DALayer工厂类
+    /// </summary>
+    public abstract class DALFactory
+    {
+        // 数据库名集合
+        private static List<string> dbNames = new List<string>();
+
+        /// <summary>
+        /// 创建DALLayer 实例
+        /// </summary>
+        /// <param name="dbPath">数据库绝对路径</param>
+        /// <param name="password">密码</param>
+        /// <param name="entityDllName">实体程序集DLL名称</param>
+        /// <param name="dbVersion">数据库版本号</param>
+        /// <param name="longConnection">是否长连接</param>
+        /// <returns></returns>
+        public static IDALayer CreateLayer(string dbPath, string password, string entityDllName, int dbVersion,bool longConnection)
+        {
+            return new DALayer(dbPath, password, entityDllName, dbVersion, longConnection, new DbProvider());
+        }
+
+
+
+    }
+}
